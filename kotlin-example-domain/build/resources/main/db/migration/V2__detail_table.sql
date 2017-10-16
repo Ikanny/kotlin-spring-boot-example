c8eab4185e8d7a0d6a0189caefbe7da5fa75fbb5
류익한 <lyuighan@lyuighan-ui-MacBook-Pro.local>
2017-10-16 11:06:20 +0900

    remove build files

--- kotlin-example-domain/build/resources/main/db/migration/V2__detail_table.sql
+++
@@ -1,9 +0,0 @@
-create table my_first_record_details (
-  id bigint not null auto_increment,
-  recordId bigint not null,
-  description varchar(255) not null,
-  createdAt datetime not null default now(),
-  modifiedAt datetime not null default now(),
-  primary key(id),
-  index(recordId)
-)
